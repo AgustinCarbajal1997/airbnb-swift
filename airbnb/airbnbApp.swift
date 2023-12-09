95e1e7588c6524d61f772fa0068242e255ea9a5c
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-12-09 20:37:46 -0300

    Initial Commit

---
+++ airbnb/airbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnbApp.swift
+//  airbnb
+//
+//  Created by Agustin Carbajal on 09/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct airbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
