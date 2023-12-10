dd647bcff3ba4e67342b98845cc1e593f06c01a4
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-12-09 22:17:44 -0300

    explore view - detail view

---
+++ airbnb/Core/Components/ListingImageCarouselView.swift
@@ -0,0 +1,20 @@
+//
+//  ListingImageCarouselView.swift
+//  airbnb
+//
+//  Created by Agustin Carbajal on 09/12/2023.
+//
+
+import SwiftUI
+
+struct ListingImageCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListingImageCarouselView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListingImageCarouselView()
+    }
+}
---
+++ airbnb/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,20 @@
+//
+//  SearchAndFilterBar.swift
+//  airbnb
+//
+//  Created by Agustin Carbajal on 09/12/2023.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchAndFilterBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchAndFilterBar()
+    }
+}
