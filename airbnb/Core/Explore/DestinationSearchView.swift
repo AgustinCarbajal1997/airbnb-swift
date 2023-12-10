da63c041c36dd45d341d302cec66edbf89619b8b
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-12-10 17:05:35 -0300

    [feature] search view

--- airbnb/Core/Explore/DestinationSearchView.swift
+++ airbnb/Core/Explore/DestinationSearchView.swift
@@ -7,14 +7,177 @@
 
 import SwiftUI
 
+enum DestinationSearchOptions {
+    case location
+    case dates
+    case guests
+}
+
 struct DestinationSearchView: View {
+    
+    @Binding var show: Bool
+    @State private var destination = ""
+    @State private var selectedOption: DestinationSearchOptions = .location
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+    @State private var numGuests = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button {
+                    withAnimation(.easeInOut) {
+                        show.toggle()
+                    }
+                } label: {
+                    Image(systemName: "xmark.circle")
+                        .imageScale(.large)
+                        .foregroundStyle(.black)
+            }
+                Spacer()
+                
+                if !destination.isEmpty {
+                    Button("Clear") {
+                        destination = ""
+                    }
+                    .foregroundStyle(.black)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                }
+            }
+            .padding()
+            
+            
+            
+            VStack(alignment: .leading) {
+                if selectedOption == .location {
+                    Text("Where to?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .imageScale(.small)
+                        TextField("Search destinations", text: $destination)
+                            .font(.subheadline)
+                    }
+                    .frame(height: 44)
+                    .padding(.horizontal)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(lineWidth: 1.0)
+                            .foregroundStyle(Color(.systemGray4))
+                    }
+                } else {
+                    CollapsePickerView(title: "Where", description: "Add destinations")
+                }
+            }
+            .modifier(CollapsableDestinationViewModifier())
+            .frame(height: selectedOption == .location ? 120 : 64)
+            .onTapGesture {
+                withAnimation(.easeInOut(duration: 0.1)) {
+                    selectedOption = .location
+                }
+            }
+            
+            
+            //date selection
+            
+            VStack(alignment: .leading) {
+                if selectedOption == .dates {
+                    
+                        Text("When's your trip")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            
+                        VStack {
+                            DatePicker("From", selection: $startDate, displayedComponents: .date)
+                            
+                            Divider()
+                            
+                            DatePicker("To", selection: $endDate, displayedComponents: .date)
+                        }
+                        .foregroundStyle(.gray)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                    
+                } else {
+                    CollapsePickerView(title: "When", description: "Add dates")
+                        
+                }
+            }
+            .modifier(CollapsableDestinationViewModifier())
+            .frame(height: selectedOption == .dates ? 180 : 64)
+            .onTapGesture {
+                withAnimation(.easeInOut(duration: 0.1)) {
+                    selectedOption = .dates
+                }
+            }
+            
+            
+            //num guests
+            VStack(alignment: .leading) {
+                if selectedOption == .guests {
+                    Text("Who's coming?")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                    
+                    Stepper {
+                        Text("\(numGuests) adults")
+                    } onIncrement: {
+                        numGuests += 1
+                    } onDecrement: {
+                        guard numGuests > 0 else { return }
+                        numGuests -= 1
+                    }
+                } else {
+                    CollapsePickerView(title: "Who", description: "Add guests")
+                }
+            }
+            .modifier(CollapsableDestinationViewModifier())
+            .frame(height: selectedOption == .guests ? 120 : 64)
+            .onTapGesture {
+                withAnimation(.easeInOut(duration: 0.1)) {
+                    selectedOption = .guests
+                }
+            }
+            Spacer()
+        }
     }
 }
 
 struct DestinationSearchView_Previews: PreviewProvider {
     static var previews: some View {
-        DestinationSearchView()
+        DestinationSearchView(show: .constant(false))
+    }
+}
+
+struct CollapsableDestinationViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+    }
+}
+
+struct CollapsePickerView: View {
+    let title: String
+    let description: String
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .foregroundStyle(.gray)
+                
+                Spacer()
+                
+                Text(description)
+            }
+            .fontWeight(.semibold)
+            .font(.subheadline)
+        }
     }
 }
